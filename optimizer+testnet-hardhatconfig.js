d36ec2f5bff87f9072d4cd8d5af8d4d75f0a5ba1
AsfndAmin <99183246+AsfndAmin@users.noreply.github.com>
2022-07-19 14:51:26 +0500

    Create optimizer+testnet-hardhatconfig.js

---
+++ optimizer+testnet-hardhatconfig.js
@@ -0,0 +1,48 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// eslint-disable-next-line no-undef
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  solidity: {
+    version: "0.8.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+        rinkeby: {
+          url: "https://rinkeby.infura.io/v3/1331c59dd4a547b3a80c9c282063bed4" ,
+          accounts:["a40289fe12e7209a24790db12ea43fa0bd58a0c5c0741e98fc7b271df7afd148"],
+        },
+        ropsten: {
+          url: "https://ropsten.infura.io/v3/1331c59dd4a547b3a80c9c282063bed4" ,
+          accounts:["a40289fe12e7209a24790db12ea43fa0bd58a0c5c0741e98fc7b271df7afd148"],
+        },
+      },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+};
