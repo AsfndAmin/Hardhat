d2863af542bac120e78a4b9ed74f9daebc21fe2e
AsfndAmin <99183246+AsfndAmin@users.noreply.github.com>
2022-07-27 16:58:20 +0500

    Create HardhatCommands.js

---
+++ HardhatCommands.js
@@ -0,0 +1,61 @@
+npm install @openzeppelin/contracts
+npm install @openzeppelin/contracts-upgradeable
+
+await expect(token.transfer(walletTo.address, 7))
+	.to.emit(token, 'Transfer')
+	.withArgs(wallet.address, walletTo.address, 7);
+
+	
+await expect(token.transfer(walletTo.address, 1007)).to.be.reverted;
+
+
+   await ERC721A.publicMint(1, {
+        value: ethers.utils.parseEther("0.000000000000001")
+    }); to send ether with a function
+
+
+when cloning a project from long age this can solve problems
+ npm install --legacy-peer-deps
+
+test/token/ERC20/extensions/ERC20Votes.test.js 
+
+
+
+for optimizer
+module.exports = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+
+ https://chakra-ui.com/docs/getting-st...
+npm i @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6
+
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+https://www.youtube.com/watch?v=ynFNLBP2TPs
+
+
+
+DEOPLY
+
+npx hardhat run scripts/deploy.js
+npx hardhat run scripts/deploy.js --network rinkeby
+
+
+verify
+npx hardhat verify --network rinkeby 0xc33287AdD16f9c9b982Ed01799cfE9e4A1D6Fc2A
+
+npx hardhat verify --network rinkeby 0xc33287AdD16f9c9b982Ed01799cfE9e4A1D6Fc2A "constructor arguments"
+
+
+to uninstall waffle
+npm uninstall @nomiclabs/hardhat-waffle ethereum-waffle
